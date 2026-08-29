477e14a93023931b237375516cacbc0e213cf19f
rafaelsouzars <rafael21.santos@hotmail.com>
2026-08-29 13:05:39 -0300

    :sparkles: feat: Download Button Description: New download button. Author: @rafaelsouzars

--- src/components/DownloadButton.tsx
+++ src/components/DownloadButton.tsx
@@ -1,11 +1,23 @@
+import type { PropsWithChildren } from 'preact/compat';
 import { FaWindows } from 'react-icons/fa';
 
-export function DownloadButton ({ children }) {
+interface DownloadButtonProps {
+  className?: string;
+  url?: string;  
+}
+
+export function DownloadButton ({ 
+  children, 
+  className = "download-button",
+  url = "#", 
+}:PropsWithChildren<DownloadButtonProps>) {
   return (
     <>
-      <button className="download-button button">
-        <FaWindows/>
-        { children }
+      <button className={className}>
+        <a href={url} target="_blank" rel="noopener noreferrer">
+          <FaWindows/>
+          { children }
+        </a>        
       </button>
     </>
   )
